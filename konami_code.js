7475d62e171e1004821a3184bc5c1aee84b0cabb
ahjuice <ahjuice@users.noreply.github.com>
2018-06-10 20:00:12 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,22 @@ const codes = [
 
 function init() {
   // your code here
+  const body = document.body;
+
+  let index = 0;
+
+  function codeListener(e) {
+    if (e.key === codes[index]) {
+      console.log(e.key)
+      index++;
+      if (index === codes.length) {
+        alert("You're pretty good.")
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
+
+  body.addEventListener('keydown', codeListener);
 }
